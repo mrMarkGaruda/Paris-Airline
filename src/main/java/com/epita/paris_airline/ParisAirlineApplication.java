bd427884012acfd9559da343b170d005f4307eea
Mark Abramenko <mark.abramenko@epita.fr>
2024-10-23 10:54:00 +0200

    Start

---
+++ src/main/java/com/epita/paris_airline/ParisAirlineApplication.java
@@ -0,0 +1,13 @@
+package com.epita.paris_airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParisAirlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParisAirlineApplication.class, args);
+    }
+
+}
